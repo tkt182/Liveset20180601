48138d183417d0ccff4e5ca1b6e1169c6a3574ee
Shinichiro Takata <shinichiro-takata@united.jp>
2018-08-23 22:25:46 +0900

    add effect

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -332,6 +332,7 @@ void ofApp::keyPressed(int key){
     if (key == 'u') myGlitch.toggleFx(OFXPOSTGLITCH_NOISE);
     if (key == 'i') myGlitch.toggleFx(OFXPOSTGLITCH_SLITSCAN);
     if (key == 'o') myGlitch.toggleFx(OFXPOSTGLITCH_SWELL);
+    if (key == 'l') myGlitch.toggleFx(OFXPOSTGLITCH_SHAKE_CUT_SLIDER);
     //if (key == 'p') myGlitch.toggleFx(OFXPOSTGLITCH_INVERT);
     //if (key == 'p') myGlitch.toggleFx(OFXPOSTGLITCH_CR_RLINE);
     //if (key == 'p') myGlitch.toggleFx(OFXPOSTGLITCH_CR_MIRROR4);
